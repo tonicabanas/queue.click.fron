accfab19925489919a125774e4e66795c88e4fc2
Antonio Cabanas <acabanas@mrmilu.com>
2018-01-09 00:58:52 +0100

    Dynamic logarithm height using time in events

--- src/js/components/Queue/Event/Event.jsx
+++ src/js/components/Queue/Event/Event.jsx
@@ -2,10 +2,25 @@ import React, {Component} from 'react';
 
 import './style.scss';
 
+const logFactor = 45.166626117938186;
+const pixelReference = 10;
+const minSize = 115;
+
 export default class Event extends Component {
+  constructor(props) {
+    super(props);
+    this.heightRems = (this.calculateHeight() / pixelReference).toString().concat('rem');
+  }
+
+  calculateHeight() {
+    return Math.max(minSize, (Math.log2(this.props.time) * logFactor));
+  }
+
   render() {
+    const styleHeight = {height: this.heightRems};
+
     return (
-      <div className='Event'>
+      <div className='Event' style={styleHeight}>
         <h1 className='Event__user'>{this.props.username}</h1>
         <div className='Event__description'>{this.props.description}</div>
         <div className='Event__time'>{this.props.time}<span className='Event__min'> min</span></div>
--- src/js/views/App/index.jsx
+++ src/js/views/App/index.jsx
@@ -16,6 +16,15 @@ export default class App extends Component {
         <div className="Events">
           <Event username="acabanas" description="Tomar un cafecito que será un día duro" time="10"/>
           <Event username="dseijo" description="Qué son los services?" time="30"/>
+          <Event username="dseijo" description="Qué son los services?" time="100"/>
+          <Event username="dseijo" description="Qué son los services?" time="120"/>
+          <Event username="dseijo" description="Qué son los services?" time="20"/>
+          <Event username="dseijo" description="Qué son los services?" time="40"/>
+          <Event username="dseijo" description="Qué son los services?" time="80"/>
+          <Event username="dseijo" description="Qué son los services?" time="180"/>
+          <Event username="dseijo" description="Qué son los services?" time="30"/>
+          <Event username="dseijo" description="Qué son los services?" time="30"/>
+          <Event username="dseijo" description="Qué son los services?" time="30"/>
         </div>
       </div>
     );
